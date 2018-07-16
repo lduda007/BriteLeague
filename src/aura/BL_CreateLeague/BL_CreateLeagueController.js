f913e9a941bc683bfaaa2ad14b1af46f429d1324
voocashbnk <lukasz.bak@britent.com.pl>
2018-07-16 14:25:43 +0200

    #16 - Create new League - fix

--- src/aura/BL_CreateLeague/BL_CreateLeagueController.js
+++ src/aura/BL_CreateLeague/BL_CreateLeagueController.js
@@ -1,5 +1,50 @@
-/**
- * Created on 13.07.2018.
- */
 ({
+    doInit: function(component, event, helper) {
+        helper.initializeNewLeagueRecord(component);
+    },
+
+    handleSaveLeague: function(component, event, helper) {
+        if(helper.validateLeagueForm(component)) {
+            if(component.find("unlimitedTeamsCheckbox").get("v.checked") == true){
+                component.set("v.simpleNewLeague.Max_Teams__c", 0 )
+            }
+            component.find("leagueRecordCreator").saveRecord(function(saveResult) {
+                if (saveResult.state === "SUCCESS" || saveResult.state === "DRAFT") {
+                    // record is saved successfully
+                    var resultsToast = $A.get("e.force:showToast");
+                    resultsToast.setParams({
+                        "type": "success",
+                        "message": "The league was created."
+                    });
+                    resultsToast.fire();
+                    helper.initializeNewLeagueRecord(component);
+
+                } else if (saveResult.state === "INCOMPLETE") {
+                    // handle the incomplete state
+                    console.log("User is offline, device doesn't support drafts.");
+                } else if (saveResult.state === "ERROR") {
+                    // handle the error state
+                    console.log('Problem saving league, error: ' + JSON.stringify(saveResult.error));
+                } else {
+                    console.log('Unknown problem, state: ' + saveResult.state + ', error: ' + JSON.stringify(saveResult.error));
+                }
+            });
+        }
+    },
+    handleVisibilityChange: function(component, event, helper){
+        let visibility = event.getSource().get("v.value");
+        if(visibility == 'public'){
+            component.set("v.simpleNewLeague.isPrivate__c", false);
+        }else{
+            component.set("v.simpleNewLeague.isPrivate__c", true);
+        }
+    },
+    handleMaxTeamsChange: function(component, event, helper){
+        component.set("v.simpleNewLeague.Max_Teams__c", event.getSource().get("v.value"));
+        console.log(JSON.stringify(component.get("v.simpleNewLeague")));
+    },
+    handleUnlimitedChanged: function(component, event, helper){
+        component.set("v.isFormVisible", false);
+        component.set("v.isFormVisible", true);
+    },
 })
--- src/aura/BL_CreateLeague/BL_CreateLeagueHelper.js
+++ src/aura/BL_CreateLeague/BL_CreateLeagueHelper.js
@@ -1,5 +1,49 @@
-/**
- * Created on 13.07.2018.
- */
 ({
+     validateLeagueForm: function(component) {
+        var validLeague = true;
+         // Show error messages if required fields are blank
+        var allValid = component.find('leagueField').reduce(function (validFields, inputCmp) {
+            inputCmp.showHelpMessageIfInvalid();
+            return validFields && inputCmp.get('v.validity').valid;
+        }, true);
+        if (allValid) {
+            return(validLeague);
+        }
+    },
+    fetchMaxTeamsPickListVal: function(component){
+        var action = component.get("c.getLeagueFieldPicklistValues");
+        action.setParams({
+            "fieldName": "Max_Team_No__c"
+        });
+        action.setCallback(this, function(response) {
+            if (response.getState() == "SUCCESS") {
+                var allValues = response.getReturnValue();
+                component.set("v.maxTeamsOptions", allValues);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    loadCurrentDate: function(component){
+        var currentDate = new Date();
+        var dateString = currentDate.getFullYear()+'-'+(currentDate.getMonth()+1)+'-'+currentDate.getDate();
+        return dateString;
+    },
+    initializeNewLeagueRecord: function(component){
+        var helper = this;
+        var currentDate = new Date();
+        component.find("leagueRecordCreator").getNewRecord(
+            "BL_League__c", // sObject type (objectApiName)
+            null,      // recordTypeId
+            false,     // skip cache?
+            $A.getCallback(function() {
+                var rec = component.get("v.newLeague");
+                var error = component.get("v.newLeagueError");
+                if(error || (rec === null)) {
+                    console.log("Error initializing record template: " + error);
+                    return;
+                }
+                component.set("v.currentDate", helper.loadCurrentDate(component));
+            })
+        );
+    },
 })
