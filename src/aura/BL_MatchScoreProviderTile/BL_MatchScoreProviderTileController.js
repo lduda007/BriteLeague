b0a7a91b9b8bd5fae81748155c142857167d4e6e
Norbert Wieruszewski <norbert.wieruszewski@britenet.com.pl>
2019-02-11 20:57:06 +0100

    Fixed match logging after Dates updated to Datetimes.

--- src/aura/BL_MatchScoreProviderTile/BL_MatchScoreProviderTileController.js
+++ src/aura/BL_MatchScoreProviderTile/BL_MatchScoreProviderTileController.js
@@ -1,86 +1,74 @@
 ({
-    doInit: function(component, event, helper){
+    doInit: function(component, event, helper) {
 
     },
 
-    handleRoundScoreSelected: function(component, event, helper){
+    handleRoundScoreSelected: function(component, event, helper) {
         let round = parseInt(event.getParam("round"));
         let givenTeam1Goals = parseInt(event.getParam("team1score"));
         let givenTeam2Goals = parseInt(event.getParam("team2score"));
         let team1TotalGoals = component.get("v.team1Goals");
         let team2TotalGoals = component.get("v.team2Goals");
-        team1TotalGoals[round-1] = givenTeam1Goals;
-        team2TotalGoals[round-1] = givenTeam2Goals;
+        team1TotalGoals[round - 1] = givenTeam1Goals;
+        team2TotalGoals[round - 1] = givenTeam2Goals;
         component.set("v.team1Goals", team1TotalGoals);
         component.set("v.team2Goals", team2TotalGoals);
-
     },
-    handleTeamGoalsChange: function(component, event, helper){
+
+    handleTeamGoalsChange: function(component, event, helper) {
         helper.updateFinalScore(component, event);
     },
-    handleSaveScore: function(component, event, helper){
+
+    handleSaveScore: function(component, event, helper) {
         let team1score = component.get("v.team1Score");
         let team2score = component.get("v.team2Score");
         component.getEvent("scoreSaved").setParams({
-            "team1score" : team1score,
-            "team2score" : team2score,
-            "team1Goals" : component.get("v.team1Goals"),
-            "team2Goals" : component.get("v.team2Goals"),
-            "matchDate" : new Date(component.get("v.matchDate")).toJSON()
+            "team1score": team1score,
+            "team2score": team2score,
+            "team1Goals": component.get("v.team1Goals"),
+            "team2Goals": component.get("v.team2Goals"),
+            "matchDate": new Date(component.get("v.matchDate")).toJSON()
         }).fire();
 
         helper.clearOldDataOnModal(component);
         helper.setTeamPanelStyle(component);
 
     },
-    handleTeamScoreChange: function(component, event, helper){
+
+    handleTeamScoreChange: function(component, event, helper) {
         helper.setTeamPanelStyle(component);
         let team1score = component.get("v.team1Score");
         let team2score = component.get("v.team2Score");
         let team1goals = component.get("v.team1Goals");
         let team2goals = component.get("v.team2Goals");
         let scoreSum = parseInt(team1score) + parseInt(team2score);
-        if(parseInt(team1score) == 1 && parseInt(team2score) == 1){
+        if(parseInt(team1score) == 1 && parseInt(team2score) == 1) {
             component.set("v.isMatchWithExtraTimeRound", true);
-        }else if(scoreSum == 3 && parseInt(team1score) != 3 && parseInt(team2score) != 3){
+        } else if(scoreSum == 3 && parseInt(team1score) != 3 && parseInt(team2score) != 3) {
             component.set("v.isMatchWithExtraTimeRound", true);
-            if( (parseInt(team1score) == 1 && parseInt(team1goals[2])==10) || (parseInt(team2score) == 1 && parseInt(team2goals[2])==10) ){
+            if((parseInt(team1score) == 1 && parseInt(team1goals[2]) == 10) || (parseInt(team2score) == 1 && parseInt(team2goals[2]) == 10)) {
                 helper.updateFinalScoreFromTwoRounds(component);
                 component.set("v.isMatchWithExtraTimeRound", false);
             }
-        }else if(parseInt(team1score)==3 || parseInt(team2score)==3){
+        } else if(parseInt(team1score) == 3 || parseInt(team2score) == 3) {
             helper.updateFinalScoreFromTwoRounds(component);
             component.set("v.isMatchWithExtraTimeRound", false);
         }
     },
-    clearData: function(component, event, helper){
+
+    clearData: function(component, event, helper) {
         helper.clearTeamRoundsScores(component);
         helper.clearTeamCardsColorsStyles(component);
-        helper.fetchMatchDateToCurrentDate(component);
         component.set("v.isMatchWithExtraTimeRound", false);
-        if(component.get("v.team1Score") != null && component.get("v.team2Score") != null){
+        if(component.get("v.team1Score") != null && component.get("v.team2Score") != null) {
             component.set("v.team2Score", 0);
             component.set("v.team1Score", 0);
             component.set("v.team1Goals", []);
             component.set("v.team2Goals", []);
         }
     },
-    handleMatchRecordChange: function(component, event, helper){
-        component.set("v.dateInputVisible", false);
-        component.set("v.dateInputVisible", true);
-//        helper.fetchMatchDateToCurrentDate(component);
-        helper.matchMaxMinDateInit(component);
-    },
 
-    matchDateChange: function(component, event, helper){
-        let selectedDate = component.get('v.matchDate');
-        helper.validateMaxMinMatchDate(component, selectedDate)
-    },
-    handleDateInputError: function(component, event, helper){
-//        console.log('error handler: '+ JSON.stringify(event.getParam('errors')));
-    },
-    handleDateInputErrorClear: function(component, event, helper){
-//        console.log('error cleared '+ JSON.stringify(event.getParams()));
-    },
-
-})
+    handleMatchRecordChange: function(component, event, helper) {
+        component.set("v.matchDate", helper.convertDateToString(component, new Date()));
+    }
+});
--- src/aura/BL_MatchesTable/BL_MatchesTableController.js
+++ src/aura/BL_MatchesTable/BL_MatchesTableController.js
@@ -1,29 +1,32 @@
 ({
-    goToTeamPage: function (component, event, helper) {
+    goToTeamPage: function(component, event, helper) {
         let recordId = event.currentTarget.dataset.recordid;
         let urlEvent = $A.get("e.force:navigateToURL");
         urlEvent.setParams({
-          "url": "/bl-team/"+recordId
+            "url": "/bl-team/" + recordId
         });
         urlEvent.fire();
     },
-    goToMatchPage: function (component, event, helper) {
+
+    goToMatchPage: function(component, event, helper) {
         let recordId = event.currentTarget.dataset.recordid;
         let urlEvent = $A.get("e.force:navigateToURL");
         urlEvent.setParams({
-          "url": "/bl-match/"+recordId
+            "url": "/bl-match/" + recordId
         });
         urlEvent.fire();
     },
-    goToLeaguePage: function (component, event, helper) {
+
+    goToLeaguePage: function(component, event, helper) {
         var recordId = event.currentTarget.dataset.recordid;
         var urlEvent = $A.get("e.force:navigateToURL");
         urlEvent.setParams({
-          "url": "/bl-league/"+recordId
+            "url": "/bl-league/" + recordId
         });
         urlEvent.fire();
     },
-    openScoreViewModal: function (component, event, helper){
+
+    openScoreViewModal: function(component, event, helper) {
         component.set("v.selectedMatchId", event.currentTarget.dataset.recordid);
         let scoreModal = component.find("scoreModal");
         component.set("v.editMode", false);
@@ -32,17 +35,17 @@
         let teamSize = matches[matchIndex].League__r.TeamSize__c;
         component.set("v.teamSize", teamSize);
         component.set('v.selectedMatchObject', matches[matchIndex]);
-
-        if(teamSize === 'Single Player'){
+        if(teamSize === 'Single Player') {
             scoreModal.set("v.size", 'small');
-        }else{
+        } else {
             scoreModal.set("v.size", 'large');
         }
         component.set("v.modalVisible", false);
         component.set("v.modalVisible", true);
         scoreModal.show();
     },
-    openScoreProviderModal: function (component, event, helper){
+
+    openScoreProviderModal: function(component, event, helper) {
         let scoreModal = component.find("scoreModal");
         let matches = component.get('v.matches');
         let matchIndex = event.getSource().get("v.value");
@@ -51,21 +54,23 @@
         component.set("v.teamSize", teamSize);
         component.set("v.selectedMatchId", match.Id);
         component.set("v.editMode", true);
-        if(teamSize === 'Single Player'){
+        if(teamSize === 'Single Player') {
             scoreModal.set("v.size", 'small');
-        }else{
+        } else {
             scoreModal.set("v.size", 'large');
         }
         component.set("v.modalVisible", false);
         component.set("v.modalVisible", true);
         scoreModal.show();
     },
-    handleScoreSelected: function (component, event, helper){
+
+    handleScoreSelected: function(component, event, helper) {
         let scoreModal = component.find("scoreModal");
         scoreModal.hide();
     },
-    onSinglePlayerMatchScoreSaved : function(component, event) {
+
+    onSinglePlayerMatchScoreSaved: function(component, event) {
         let scoreModal = component.find("scoreModal");
         scoreModal.hide();
     }
-})
+});
