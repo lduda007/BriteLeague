ce15f531eaf9e89eb05ef603ebbeb60c8af4cde3
Norbert Wieruszewski <norbert.wieruszewski@britenet.com.pl>
2019-03-31 12:11:29 +0200

    battle-royale-mode - Updated "CreateLeague" component

--- src/aura/BL_CreateLeague/BL_CreateLeagueController.js
+++ src/aura/BL_CreateLeague/BL_CreateLeagueController.js
@@ -3,84 +3,45 @@
         helper.doInit(component);
     },
 
-    onSave: function(component, event, helper) {
-        if(helper.validateLeagueForm(component, event)) {
-            let competitionType = component.get("v.competitionType");
-            let initDataWrapper = component.get("v.initDataWrapper");
-            component.set("v.simpleNewLeague.RecordTypeId", initDataWrapper.leagueRecordTypeDeveloperNameToId[competitionType]);
-
-            if(competitionType === "Cup") {
-                component.set("v.simpleNewLeague.Match_No__c", 1);
-            }
-            let teamSize = component.get("v.teamSize");
-            component.set("v.simpleNewLeague.TeamSize__c", teamSize);
-            component.find("leagueRecordCreator").saveRecord(function(saveResult) {
-                if(saveResult.state === "SUCCESS" || saveResult.state === "DRAFT") {
-                    // record is saved successfully
-                    let resultsToast = $A.get("e.force:showToast");
-                    resultsToast.setParams({
-                        "type": "success",
-                        "message": "The competition was created."
-                    });
-                    resultsToast.fire();
-                    helper.initializeNewLeagueRecord(component);
-                    let appEvent = $A.get("e.c:BL_CompetitionCreatedEvent");
-                    appEvent.fire();
-                } else if(saveResult.state === "INCOMPLETE") {
-                    // handle the incomplete state
-                    console.log("User is offline, device doesn't support drafts.");
-                } else if(saveResult.state === "ERROR") {
-                    // handle the error state
-                    console.log('Problem saving league, error: ' + JSON.stringify(saveResult.error));
-                } else {
-                    console.log('Unknown problem, state: ' + saveResult.state + ', error: ' + JSON.stringify(saveResult.error));
-                }
-            });
-        } else {
-            let resultsToast = $A.get("e.force:showToast");
-            resultsToast.setParams({
-                "type": "error",
-                "message": "Please provide valid data"
-            }).fire();
-        }
-    },
-
-    handleVisibilityChange: function(component, event, helper) {
-        let visibility = event.getSource().get("v.value");
-        if(visibility == 'public') {
-            component.set("v.simpleNewLeague.isPrivate__c", false);
-        } else {
-            component.set("v.simpleNewLeague.isPrivate__c", true);
-        }
-    },
-
-    handleMaxTeamsChange: function(component, event, helper) {
-        let validity = component.find("leagueField");
-        for(let ii = 0; ii < validity.length; ii++) {
-            let validation = validity[ii].get("v.validity");
-        }
-        component.set("v.simpleNewLeague.Match_No__c", null);
-        helper.fetchLeagueRoundsList(component);
-    },
-
-    handleStartDateChange: function(component, event, helper) {
-        helper.calculateMaxEndDate(component);
-    },
-
-    handleUnlimitedChanged: function(component, event, helper) {
-        component.set("v.isFormVisible", false);
-        component.set("v.isFormVisible", true);
-    },
-
-    handleNoOfRoundsOrTeamsChange: function(component, event, helper) {
-        helper.calculateTotalMatches(component);
-    },
-
-    handleClear: function(component, event, helper) {
-        helper.clearForm(component);
-    },
-
     onLeagueRecordTypeChange: function(component, event, helper) {
         helper.clearForm(component);
+    },
+
+    onSave: function(component, event, helper) {
+        helper.doSave(component);
+
+        // if(helper.validateLeagueForm(component, event)) {
+        //     let competitionType = component.get("v.competitionType");
+        //     let initDataWrapper = component.get("v.initDataWrapper");
+        //     component.set("v.simpleNewLeague.RecordTypeId", initDataWrapper.leagueRecordTypeDeveloperNameToId[competitionType]);
+        //
+        //     if(competitionType === "Cup") {
+        //         component.set("v.simpleNewLeague.Match_No__c", 1);
+        //     }
+        //     let teamSize = component.get("v.teamSize");
+        //     component.set("v.simpleNewLeague.TeamSize__c", teamSize);
+        //     component.find("leagueRecordCreator").saveRecord(function(saveResult) {
+        //         if(saveResult.state === "SUCCESS" || saveResult.state === "DRAFT") {
+        //             // record is saved successfully
+        //             let resultsToast = $A.get("e.force:showToast");
+        //             resultsToast.setParams({
+        //                 "type": "success",
+        //                 "message": "The competition was created."
+        //             });
+        //             resultsToast.fire();
+        //             helper.initializeNewLeagueRecord(component);
+        //             let appEvent = $A.get("e.c:BL_CompetitionCreatedEvent");
+        //             appEvent.fire();
+        //         } else if(saveResult.state === "INCOMPLETE") {
+        //             // handle the incomplete state
+        //             console.log("User is offline, device doesn't support drafts.");
+        //         } else if(saveResult.state === "ERROR") {
+        //             // handle the error state
+        //             console.log('Problem saving league, error: ' + JSON.stringify(saveResult.error));
+        //         } else {
+        //             console.log('Unknown problem, state: ' + saveResult.state + ', error: ' + JSON.stringify(saveResult.error));
+        //         }
+        //     });
+        // }
     }
 });
--- src/aura/BL_CreateLeague/BL_CreateLeagueHelper.js
+++ src/aura/BL_CreateLeague/BL_CreateLeagueHelper.js
@@ -4,11 +4,14 @@
         action.setCallback(this, function(response) {
             let state = response.getState();
             let result = response.getReturnValue();
+            let errors = response.getError();
 
             if(state === "SUCCESS") {
+                let newLeagueWrapper = result.leagueWrapper;
+                newLeagueWrapper.startDate = this.convertDateToString(component, new Date());
+                component.set("v.newLeagueWrapper", newLeagueWrapper);
                 component.set("v.initDataWrapper", result);
             } else if(state === "ERROR") {
-                let errors = response.getError();
                 let message = 'Unknown error';
                 if(errors && Array.isArray(errors) && errors.length > 0) {
                     message = errors[0].message;
@@ -19,120 +22,62 @@
         $A.enqueueAction(action);
     },
 
-    validateLeagueForm: function(component, event) {
-        var allValid = component.find('leagueField').reduce(function(validSoFar, inputCmp) {
-            inputCmp.reportValidity();
-            return validSoFar && inputCmp.checkValidity();
-        }, true);
-        if(allValid) {
-            return true
-        } else {
-            return false
-        }
-    },
-
-    fetchMaxTeamsPickListVal: function(component) {
-        let action = component.get("c.getLeagueFieldPicklistValues");
+    doSave: function(component) {
+        let action = component.get("c.saveCompetition");
         action.setParams({
-            "fieldName": "Max_Team_No__c"
+            newLeagueWrapper: JSON.stringify(component.get("v.newLeagueWrapper"))
         });
         action.setCallback(this, function(response) {
-            if(response.getState() == "SUCCESS") {
-                var allValues = response.getReturnValue();
-                component.set("v.maxTeamsOptions", allValues);
+            let state = response.getState();
+            let result = response.getReturnValue();
+            let errors = response.getError();
+
+            if(state === "SUCCESS") {
+                this.showToast('success', 'Competiton Created!');
+            } else if(state === "ERROR") {
+                let message = 'Unknown error';
+                if(errors && Array.isArray(errors) && errors.length > 0) {
+                    message = errors[0].message;
+                }
+                console.error(message);
             }
         });
         $A.enqueueAction(action);
     },
 
-    loadCurrentDate: function(component) {
-        let currentDate = new Date();
-        let currentDateMonth = (currentDate.getMonth() + 1);
-        let currentDateDays = currentDate.getDate();
-        if(parseInt(currentDateMonth) < 10) {
-            currentDateMonth = '0' + currentDateMonth;
-        }
-        if(parseInt(currentDateDays) < 10) {
-            currentDateDays = '0' + currentDateDays;
-        }
-        let dateString = currentDate.getFullYear() + '-' + currentDateMonth + '-' + currentDateDays;
-//        let dateString = currentDate.getFullYear()+'-'+(currentDate.getMonth()+1)+'-'+currentDate.getDate();
-        let yearLater = currentDate;
-        yearLater.setDate(yearLater.getDate() + 365);
-        let yearLaterString = yearLater.getFullYear() + '-' + (yearLater.getMonth() + 1) + '-' + yearLater.getDate();
-        component.set("v.yearLaterDate", yearLaterString);
-        component.set("v.currentDate", dateString);
-        return dateString;
-    },
+    convertDateToString: function(component, dateValue) {
+        let dateObject = new Date(Date.parse(dateValue));
+        let dateObjectMonth = (dateObject.getMonth() + 1);
+        let dateObjectDay = dateObject.getDate();
 
-    calculateMaxEndDate: function(component) {
-        //add 365 days (31536000000 milliseconds)
-        let maxEndDate = new Date((Date.parse(component.get("v.simpleNewLeague.Start_Date__c"))) + 31536000000);
-        let maxEndDateMonth = (maxEndDate.getMonth() + 1);
-        let maxEndDateDays = maxEndDate.getDate();
-        if(parseInt(maxEndDateMonth) < 10) {
-            maxEndDateMonth = '0' + maxEndDateMonth;
+        if(parseInt(dateObjectMonth) < 10) {
+            dateObjectMonth = '0' + dateObjectMonth;
         }
-        if(parseInt(maxEndDateDays) < 10) {
-            maxEndDateDays = '0' + maxEndDateDays;
+
+        if(parseInt(dateObjectDay) < 10) {
+            dateObjectDay = '0' + dateObjectDay;
         }
-        let maxEndDateString = maxEndDate.getFullYear() + '-' + maxEndDateMonth + '-' + maxEndDateDays;
-        component.set("v.maxEndDate", maxEndDateString);
-        component.set("v.simpleNewLeague.End_Date__c", null);
-        component.set("v.endDateInputVisible", false);
-        component.set("v.endDateInputVisible", true);
-    },
 
-    loadMaxStartDate: function(component) {
-
-    },
-
-    initializeNewLeagueRecord: function(component) {
-        let helper = this;
-        let currentDate = new Date();
-        component.find("leagueRecordCreator").getNewRecord(
-            "BL_League__c", // sObject type (objectApiName)
-            null,      // recordTypeId
-            false,     // skip cache?
-            $A.getCallback(function() {
-                let rec = component.get("v.newLeague");
-                let error = component.get("v.newLeagueError");
-                if(error || (rec === null)) {
-                    console.log("Error initializing record template: " + error);
-                    return;
-                }
-            })
-        );
-    },
-
-    calculateTotalMatches: function(component) {
-        let totalTeams = component.get("v.simpleNewLeague.Max_Teams__c");
-        let totalRounds = component.get("v.simpleNewLeague.Match_No__c");
-        component.set("v.totalMatches", totalRounds * (totalTeams / 2) * (totalTeams - 1));
-    },
-
-    fetchLeagueRoundsList: function(component) {
-        let totalTeams = component.get("v.simpleNewLeague.Max_Teams__c");
-        let matchesInSingleRound = totalTeams - 1;
-        let totalMatchesLimit = component.get("v.totalMatchesLimit");
-        let roundsLimit = Math.floor(totalMatchesLimit / ((totalTeams / 2) * matchesInSingleRound));
-        if(roundsLimit > component.get("v.totalRoundsLimit")) {
-            roundsLimit = component.get("v.totalRoundsLimit");
-        }
-        let leagueRoundsList = [];
-        for(let ii = 1; ii <= roundsLimit; ii++) {
-            leagueRoundsList.push(ii);
-        }
-        component.set("v.leagueRoundsNumber", leagueRoundsList);
+        return dateObject.getFullYear()
+            + '-' + dateObjectMonth
+            + '-' + dateObjectDay
+            + 'T' + dateObject.getHours()
+            + ':' + dateObject.getMinutes()
+            + ':' + dateObject.getSeconds()
+            + '.' + dateObject.getMilliseconds() + 'Z';
     },
 
     clearForm: function(component) {
-        component.set("v.simpleNewLeague.Name", null);
-        component.set("v.simpleNewLeague.Max_Teams__c", null);
-        component.set("v.simpleNewLeague.Match_No__c", null);
-        component.set("v.simpleNewLeague.Start_Date__c", null);
-        component.set("v.simpleNewLeague.End_Date__c", null);
-        component.set("v.isFormVisible", false);
-        component.set("v.isFormVisible", true);
+        let newLeagueWrapper = component.get("v.newLeagueWrapper");
+        newLeagueWrapper.teamsLimit = null;
+        newLeagueWrapper.rounds = null;
+        component.set("v.newLeagueWrapper", newLeagueWrapper);
+    },
+
+    showToast: function(type, message) {
+        $A.get("e.force:showToast").setParams({
+            "type": type,
+            "message": message
+        }).fire();
     }
 });
