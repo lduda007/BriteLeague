fb72ea72ae5ec9f61eae1b5e7aa9b889e6d8c5de
Norbert Wieruszewski <norbert.wieruszewski@britenet.com.pl>
2019-05-06 11:50:44 +0200

    battle-royale-mode - Fix for BL_MatchesTable on BL_BattleRoyalePage

--- src/aura/BL_BattleRoyalePage/BL_BattleRoyalePageController.js
+++ src/aura/BL_BattleRoyalePage/BL_BattleRoyalePageController.js
@@ -1,28 +1,5 @@
 ({
-    getData: function(component, event, helper) {
-        let action = component.get("c.getMatchesDataWrapper");
-        let leagueId = component.get("v.leagueId");
-        action.setParam("leagueId", leagueId);
-
-        action.setCallback(this, (response) => {
-            let state = response.getState();
-            if (state === "SUCCESS") {
-                let settings = response.getReturnValue();
-                component.set("v.settings", settings);
-            } else {
-                let resultsToast = $A.get("e.force:showToast");
-                if ($A.util.isUndefined(resultsToast)){
-                    alert($A.get("$Label.c.BL_Error_when_loading_league_details"));
-                } else {
-                    resultsToast.setParams({
-                        "title": $A.get("$Label.c.BL_Error"),
-                        "message": $A.get("$Label.c.BL_Error_when_loading_league_details")
-                    });
-                    resultsToast.fire();
-                }
-            }
-            component.set("v.showSpinner", false);
-        });
-        $A.enqueueAction(action);
+    onInit: function(component, event, helper) {
+        helper.doInit(component);
     }
-})
+});
