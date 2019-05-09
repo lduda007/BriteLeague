5cb37ca60dff8b6e885628324313d1de8f390a36
Norbert Wieruszewski <norbert.wieruszewski@britenet.com.pl>
2019-05-09 17:02:11 +0200

    battle-royale-mode - Started working on new features in BL_BattleRoyaleTable component

--- src/aura/BL_BattleRoyaleTable/BL_BattleRoyaleTableController.js
+++ src/aura/BL_BattleRoyaleTable/BL_BattleRoyaleTableController.js
@@ -1,5 +1,17 @@
 ({
     onInit: function(component, event, helper) {
         helper.getDataWrapper(component);
+    },
+
+    onToggleViewMode: function(component, event, helper) {
+        let viewMode = component.get("v.viewMode");
+
+        if(viewMode === "COMPACT") {
+            viewMode = "DETAILS";
+        } else {
+            viewMode = "COMPACT";
+        }
+
+        component.set("v.viewMode", viewMode);
     }
 });
